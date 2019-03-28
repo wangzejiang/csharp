37e04c3bbdb6d405c1d40d028e6f35ee3e477583
ff <339811501@qq.com>
2019-03-28 15:32:21 +0800

    11

--- 单品关键词分析/单品关键词分析/Form1.Designer.cs
+++ 单品关键词分析/单品关键词分析/Form1.Designer.cs
@@ -28,7 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
@@ -39,8 +43,15 @@
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.button3 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
@@ -48,6 +59,9 @@
             this.tabPage2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -61,6 +75,16 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1302, 45);
             this.panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1037, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "清空";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox1
             // 
@@ -94,6 +118,7 @@
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl1.Location = new System.Drawing.Point(0, 45);
             this.tabControl1.Name = "tabControl1";
@@ -163,19 +188,98 @@
             this.textBox2.Size = new System.Drawing.Size(1288, 501);
             this.textBox2.TabIndex = 0;
             // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.chart1);
+            this.tabPage4.Controls.Add(this.panel2);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(1294, 507);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(3, 47);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1288, 457);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button7);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1288, 44);
+            this.panel2.TabIndex = 0;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(967, 10);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 4;
+            this.button7.Text = "支付金额";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(885, 10);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "支付转化率";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(803, 10);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "支付买家数";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(5, 11);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(709, 21);
+            this.textBox3.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(721, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "访客数";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(1037, 13);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "清空";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
@@ -185,7 +289,7 @@
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "关键词分析";
+            this.Text = "关键词分析(内部)";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
@@ -196,6 +300,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.tabPage3.ResumeLayout(false);
             this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -215,6 +323,14 @@
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- 单品关键词分析/单品关键词分析/Form1.cs
+++ 单品关键词分析/单品关键词分析/Form1.cs
@@ -5,11 +5,13 @@ using System.ComponentModel;
 using System.Data;
 using System.Data.OleDb;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace 单品关键词分析
 {
@@ -21,6 +23,8 @@ namespace 单品关键词分析
         }
 
         string path = @"xxx.txt";
+
+        string titleName = "";
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -35,6 +39,7 @@ namespace 单品关键词分析
                 dt.Columns.Add("访客数", typeof(float));
                 dt.Columns.Add("人均浏览量", typeof(float));
                 dt.Columns.Add("跳失率", typeof(float));
+                dt.Columns.Add("跳失人数", typeof(float)); 
                 dt.Columns.Add("支付买家数", typeof(float));
                 dt.Columns.Add("支付商品件数", typeof(float));
                 dt.Columns.Add("支付金额", typeof(float));
@@ -42,6 +47,12 @@ namespace 单品关键词分析
                 dt.Columns.Add("日期", typeof(string));
 
                 string[] fns = openFileDialog1.FileNames;
+                if(fns.Length > 0)
+                {
+                    string fpath = Path.GetFullPath(fns[0]);
+                    string[] fpaths = fpath.Split('\\');
+                    titleName = fpaths[2];
+                }
                 foreach (var f in fns)
                 {
                     DataTable rsdt = null;
@@ -61,9 +72,12 @@ namespace 单品关键词分析
                         dr["关键词"] = rs[0].ToString();
                         int i = 5;
                         dr["浏览量"] = toFloat(rs[i++]);
-                        dr["访客数"] = toFloat(rs[i++]);
+                        var 访客数 = toFloat(rs[i++]);
+                        dr["访客数"] = 访客数;
                         dr["人均浏览量"] = toFloat(rs[i++]);
-                        dr["跳失率"] = toFloat(rs[i++]);
+                        var 跳失率 = toFloat(rs[i++]);
+                        dr["跳失率"] = 跳失率;
+                        dr["跳失人数"] = 访客数 * 跳失率 / 100;
                         dr["支付买家数"] = toFloat(rs[i++]);
                         dr["支付商品件数"] = toFloat(rs[i++]);
                         dr["支付金额"] = toFloat(rs[i++]);
@@ -117,7 +131,7 @@ namespace 单品关键词分析
             {
                 using (StreamWriter sw = new StreamWriter(path, true))
                 {
-                    sw.Write(string.Format("{0}:{1}", DateTime.Now.ToLongDateString(), str));
+                    sw.Write(string.Format("{0}:{1}:{2}",titleName, DateTime.Now.ToLongDateString(), str));
                     sw.WriteLine();
                     sw.Close();
                 }
@@ -127,18 +141,18 @@ namespace 单品关键词分析
                 textBox2.Text = reader.ReadToEnd();
                 reader.Close();
             }
-
             DataTable dt = new DataTable();
             dt.Columns.Add("词", typeof(string));
             dt.Columns.Add("浏览量", typeof(float));
             dt.Columns.Add("访客数", typeof(float));
             dt.Columns.Add("人均浏览量", typeof(float));
+            dt.Columns.Add("平均跳失率", typeof(float));
             dt.Columns.Add("跳失率", typeof(float));
             dt.Columns.Add("支付买家数", typeof(float));
             dt.Columns.Add("支付商品件数", typeof(float));
             dt.Columns.Add("支付金额", typeof(float));
+            dt.Columns.Add("平均支付转化率", typeof(float));
             dt.Columns.Add("支付转化率", typeof(float));
-
             DataTable ds = (DataTable)dataGridView1.DataSource;
             if (ds == null || ds.Rows.Count == 0) return;
             string[] ss = str.Split(' ');
@@ -149,18 +163,90 @@ namespace 单品关键词分析
                 dr["浏览量"] = toFloat(ds.Compute("Sum(浏览量)", "关键词 like '%" + s + "%'"));
                 dr["访客数"] = toFloat(ds.Compute("Sum(访客数)", "关键词 like '%" + s + "%'"));
                 dr["人均浏览量"] = toFloat(ds.Compute("Sum(人均浏览量)", "关键词 like '%" + s + "%'"));
-                dr["跳失率"] = toFloat(ds.Compute("Sum(跳失率)/Count(跳失率)", "关键词 like '%" + s + "%'"));
+                dr["平均跳失率"] = toFloat(ds.Compute("Sum(跳失率)/Count(跳失率)", "关键词 like '%" + s + "%'"));
+                dr["跳失率"] = toFloat(ds.Compute("Sum(跳失人数)/Sum(访客数)*100", "关键词 like '%" + s + "%'"));
                 dr["支付买家数"] = toFloat(ds.Compute("Sum(支付买家数)", "关键词 like '%" + s + "%'"));
                 dr["支付商品件数"] = toFloat(ds.Compute("Sum(支付商品件数)", "关键词 like '%" + s + "%'"));
                 dr["支付金额"] = toFloat(ds.Compute("Sum(支付金额)", "关键词 like '%" + s + "%'"));
-                dr["支付转化率"] = toFloat(ds.Compute("Sum(支付转化率)/Count(跳失率)", "关键词 like '%" + s + "%'"));
+                dr["平均支付转化率"] = toFloat(ds.Compute("Sum(支付转化率)/Count(支付转化率)", "关键词 like '%" + s + "%'"));
+                dr["支付转化率"] = toFloat(ds.Compute("Sum(支付买家数)/Sum(访客数)*100", "关键词 like '%" + s + "%'"));
                 dt.Rows.Add(dr);
             }
             dataGridView2.DataSource = dt;
         }
 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            tj("访客数");
+        }
+
+        private void tj(string name, bool isLv = true)
+        {
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+            chart1.ChartAreas.Add(new ChartArea());
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "yyyy-MM-dd";
+            chart1.ChartAreas[0].AxisX.Interval = 1;
+            chart1.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Days;
+            chart1.ChartAreas[0].AxisX.IntervalOffset = 1;
+            if (string.IsNullOrEmpty(textBox3.Text))
+            {
+                textBox3.Text = textBox1.Text;
+            }
+            string str = textBox3.Text;
+            string[] ss = str.Split(' ');
+            foreach (string s in ss)
+            {
+                initChart(name, s, isLv);
+            }
+        }
+
+        private void initChart(string tj, string word, bool isLv = true)
+        {
+            DataTable ds = (DataTable)dataGridView1.DataSource;
+            var query = from t in ds.AsEnumerable().Where(dd => dd.Field<string>("关键词").IndexOf(word) > -1)
+                        group t by new { 日期 = t.Field<string>("日期") } into m
+                        select new
+                        {
+                            日期 = m.Key.日期,
+                            统计 = isLv ? m.Sum(n => n.Field<float>(tj)) :( m.Sum(n => n.Field<float>(tj)) / m.Sum(n => n.Field<float>("访客数")))
+                        };
+            var s = new Series();
+            s.ChartType = SeriesChartType.Line;
+            s.BorderWidth = 2;
+            s.XValueType = ChartValueType.DateTime;
+            s.Name = word;
+            var list = query.ToList();
+            DateTime dt = DateTime.Now;
+            foreach (var item in list)
+            {
+                dt = DateTime.ParseExact(item.日期, "yyyy-MM-dd", System.Globalization.CultureInfo.CurrentCulture);
+                s.Points.AddXY(dt, item.统计);
+                //Console.WriteLine(item.日期 + "\t" + item.统计);
+            }
+            //s.ToolTip = "#VALX,#VAL";
+            s.IsValueShownAsLabel = true;
+            chart1.Series.Add(s);
+            //chart1.ChartAreas[0].AxisX.Minimum = dt.AddSeconds(-1).ToOADate();
+            //chart1.ChartAreas[0].AxisX.Maximum = dt.AddSeconds(-1).AddMonths(1).ToOADate();
+        }
+
+        void chart_GetToolTipText(object sender, ToolTipEventArgs e)
+        {
+            if (e.HitTestResult.ChartElementType == ChartElementType.DataPoint)
+            {
+                int i = e.HitTestResult.PointIndex;
+                Series series = e.HitTestResult.Series;
+                DataPoint dp = series.Points[i];
+                e.Text = string.Format("{0}:{1}", series.Name, dp.YValues[0]);
+            }
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
+
+            chart1.GetToolTipText += new EventHandler<ToolTipEventArgs>(chart_GetToolTipText);
+
             if (File.Exists(path))
             {
                 using (StreamReader reader = new StreamReader(path))
@@ -175,7 +261,10 @@ namespace 单品关键词分析
                     {
                         str = reader.ReadLine();
                     }
-                    textBox1.Text = str.Split(':')[1];
+                    if(str!= null && str.Length > 0)
+                    {
+                        textBox1.Text = str.Split(':')[2];
+                    }
                     reader.Close();
                 }
             }
@@ -191,6 +280,21 @@ namespace 单品关键词分析
             ds.Clear();
             DataTable ds2 = (DataTable)dataGridView2.DataSource;
             ds2.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            tj("支付买家数");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            tj("支付转化率",false);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            tj("支付金额");
         }
     }
 }
